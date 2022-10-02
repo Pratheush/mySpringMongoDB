4da19dcd44e4d47afb35e414b1054ebeb0fa451e
Pratheush <pratheush@outlook.com>
2022-10-02 09:18:14 +0530

    first commit

---
+++ src/main/java/com/mylearning/myspringmongodb/MySpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.myspringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylearning/myspringmongodb/MySpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.myspringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
